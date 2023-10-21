897e9a3a214ba56843ab0bfa860123789c2ed7e3
unknown <alirezang7575@gmial.com>
2023-10-21 10:03:24 +0330

    added Dbcontext to our project

---
+++ ShopStoreApi/Data/Context/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ShopStoreApi.Entity.Products;
+
+namespace ShopStoreApi.Data.Context;
+
+public class StoreContext:DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<product> prooduct { get; set; }
+}
